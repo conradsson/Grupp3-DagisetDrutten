0bee07af80cded4f14856323d57c81ebc9ade098
Johan <conradsson1993@hotmail.com>
2016-08-30 16:40:23 +0200

    Drutten Grund

---
+++ Grupp3 - Förskolan Drutten/Login.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Grupp3___Förskolan_Drutten
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LoggaInButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(126, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(524, 443);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // LoggaInButton
+            // 
+            this.LoggaInButton.Location = new System.Drawing.Point(221, 204);
+            this.LoggaInButton.Name = "LoggaInButton";
+            this.LoggaInButton.Size = new System.Drawing.Size(85, 26);
+            this.LoggaInButton.TabIndex = 0;
+            this.LoggaInButton.Text = "Logga in";
+            this.LoggaInButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.White;
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.LoggaInButton);
+            this.groupBox2.Location = new System.Drawing.Point(71, 129);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(387, 273);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Grupp3___Förskolan_Drutten.Properties.Resources.drutten;
+            this.pictureBox1.Location = new System.Drawing.Point(191, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 112);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBox1.Location = new System.Drawing.Point(126, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Användarnamn";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Användarnamn:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Lösenord:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBox2.Location = new System.Drawing.Point(126, 154);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(123, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Lösenord";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button LoggaInButton;
+    }
+}
+
---
+++ Grupp3 - Förskolan Drutten/StartForalder.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Grupp3___Förskolan_Drutten
+{
+    partial class StartForalder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.inloggadSomNamnLabel = new System.Windows.Forms.Label();
+            this.inloggadSomLabel = new System.Windows.Forms.Label();
+            this.informationButton = new System.Windows.Forms.Button();
+            this.närvaroButton = new System.Windows.Forms.Button();
+            this.tiderButton = new System.Windows.Forms.Button();
+            this.mittKontoButton = new System.Windows.Forms.Button();
+            this.informationTabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.kontoTypLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.informationTabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.groupBox1.Controls.Add(this.inloggadSomNamnLabel);
+            this.groupBox1.Controls.Add(this.inloggadSomLabel);
+            this.groupBox1.Controls.Add(this.informationButton);
+            this.groupBox1.Controls.Add(this.närvaroButton);
+            this.groupBox1.Controls.Add(this.tiderButton);
+            this.groupBox1.Controls.Add(this.mittKontoButton);
+            this.groupBox1.Controls.Add(this.informationTabControl);
+            this.groupBox1.Location = new System.Drawing.Point(22, 104);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(957, 435);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Grupp3___Förskolan_Drutten.Properties.Resources.druttenMellan;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(113, 86);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // inloggadSomNamnLabel
+            // 
+            this.inloggadSomNamnLabel.AutoSize = true;
+            this.inloggadSomNamnLabel.Location = new System.Drawing.Point(97, 16);
+            this.inloggadSomNamnLabel.Name = "inloggadSomNamnLabel";
+            this.inloggadSomNamnLabel.Size = new System.Drawing.Size(85, 13);
+            this.inloggadSomNamnLabel.TabIndex = 13;
+            this.inloggadSomNamnLabel.Text = "(Användarnamn)";
+            // 
+            // inloggadSomLabel
+            // 
+            this.inloggadSomLabel.AutoSize = true;
+            this.inloggadSomLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inloggadSomLabel.Location = new System.Drawing.Point(15, 16);
+            this.inloggadSomLabel.Name = "inloggadSomLabel";
+            this.inloggadSomLabel.Size = new System.Drawing.Size(90, 13);
+            this.inloggadSomLabel.TabIndex = 12;
+            this.inloggadSomLabel.Text = "Inloggad som: ";
+            // 
+            // informationButton
+            // 
+            this.informationButton.Location = new System.Drawing.Point(18, 71);
+            this.informationButton.Name = "informationButton";
+            this.informationButton.Size = new System.Drawing.Size(163, 31);
+            this.informationButton.TabIndex = 11;
+            this.informationButton.Text = "Information";
+            this.informationButton.UseVisualStyleBackColor = true;
+            // 
+            // närvaroButton
+            // 
+            this.närvaroButton.Location = new System.Drawing.Point(18, 200);
+            this.närvaroButton.Name = "närvaroButton";
+            this.närvaroButton.Size = new System.Drawing.Size(163, 31);
+            this.närvaroButton.TabIndex = 10;
+            this.närvaroButton.Text = "Närvaro";
+            this.närvaroButton.UseVisualStyleBackColor = true;
+            // 
+            // tiderButton
+            // 
+            this.tiderButton.Location = new System.Drawing.Point(18, 157);
+            this.tiderButton.Name = "tiderButton";
+            this.tiderButton.Size = new System.Drawing.Size(163, 31);
+            this.tiderButton.TabIndex = 9;
+            this.tiderButton.Text = "Tider";
+            this.tiderButton.UseVisualStyleBackColor = true;
+            // 
+            // mittKontoButton
+            // 
+            this.mittKontoButton.Location = new System.Drawing.Point(18, 114);
+            this.mittKontoButton.Name = "mittKontoButton";
+            this.mittKontoButton.Size = new System.Drawing.Size(163, 31);
+            this.mittKontoButton.TabIndex = 8;
+            this.mittKontoButton.Text = "Mitt Konto";
+            this.mittKontoButton.UseVisualStyleBackColor = true;
+            // 
+            // informationTabControl
+            // 
+            this.informationTabControl.Controls.Add(this.tabPage1);
+            this.informationTabControl.Controls.Add(this.tabPage2);
+            this.informationTabControl.Location = new System.Drawing.Point(193, 30);
+            this.informationTabControl.Name = "informationTabControl";
+            this.informationTabControl.SelectedIndex = 0;
+            this.informationTabControl.Size = new System.Drawing.Size(748, 389);
+            this.informationTabControl.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(740, 363);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(740, 363);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // kontoTypLabel
+            // 
+            this.kontoTypLabel.AutoSize = true;
+            this.kontoTypLabel.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kontoTypLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.kontoTypLabel.Location = new System.Drawing.Point(141, 71);
+            this.kontoTypLabel.Name = "kontoTypLabel";
+            this.kontoTypLabel.Size = new System.Drawing.Size(93, 25);
+            this.kontoTypLabel.TabIndex = 4;
+            this.kontoTypLabel.Text = "Förälder";
+            // 
+            // StartForalder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SaddleBrown;
+            this.ClientSize = new System.Drawing.Size(1008, 561);
+            this.Controls.Add(this.kontoTypLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "StartForalder";
+            this.Text = "Start";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.informationTabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label inloggadSomNamnLabel;
+        private System.Windows.Forms.Label inloggadSomLabel;
+        private System.Windows.Forms.Button informationButton;
+        private System.Windows.Forms.Button närvaroButton;
+        private System.Windows.Forms.Button tiderButton;
+        private System.Windows.Forms.Button mittKontoButton;
+        private System.Windows.Forms.TabControl informationTabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label kontoTypLabel;
+    }
+}
