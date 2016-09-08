7466de76511d0bcd70ed37be901d1ca83ca46b9f
Hischam El Yahiaoui <hiel1500@student.miun.se>
2016-09-08 12:20:35 +0200

    Klassnärvaro

---
+++ Grupp3 - Förskolan Drutten/Närvaro.cs
@@ -0,0 +1,29 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Windows.Forms;
+
+
+namespace Grupp3___Förskolan_Drutten
+{
+    class Närvaro
+    {
+        public int Närvaroid { get; set; }
+        public DateTime Datum { get; set; }
+        public int Barnid { get; set; }
+        public DateTime TidLämnad { get; set; }
+        public DateTime TidHämtad { get; set; }
+        public string HämtasAv { get; set; }
+
+
+        public override string ToString()
+        {
+            return Närvaroid + " " + Datum + " " + Barnid + " " + TidLämnad + " " + TidHämtad + " " + HämtasAv;
+        }
+    }
+}
