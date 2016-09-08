584271348993c9320378d4442daaca0d1b118dc4
Johan <Johan@JOHAN-LAPTOP>
2016-09-08 17:11:31 +0200

    commit

--- Grupp3 - Förskolan Drutten/Login.cs
+++ Grupp3 - Förskolan Drutten/Login.cs
@@ -111,9 +111,7 @@ namespace Grupp3___Förskolan_Drutten
         private void TestJohanButton_Click(object sender, EventArgs e)
         {
             
-            andvandarnamnTextbox.Text = p.inskrivetAnvändarnamn;
-            losenordTextbox.Text = p.inskrivetLösenord;
-            p.KontrolleraAnvändare();
+            p.KontrolleraAnvändare(andvandarnamnTextbox.Text, losenordTextbox.Text);
 
             //if (p.inskrivetAnvändarnamn == person.Användarnamn && p.inskrivetLösenord == person.Lösenord)
             //{
