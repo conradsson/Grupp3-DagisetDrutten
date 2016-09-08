be2a2cca786f51e02cbd217fef288780b5d6a428
mathildaw <m.athildaa@hotmail.com>
2016-09-08 11:58:38 +0200

    Avdelningsklass

---
+++ Grupp3 - Förskolan Drutten/Avdelning.cs
@@ -0,0 +1,19 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Grupp3___Förskolan_Drutten
+{
+    class Avdelning
+    {
+        public int Avdelningsid { get; set; }
+        public string Namn { get; set; }
+
+    }
+}
