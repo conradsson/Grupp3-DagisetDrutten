97dccd81a5997ce3eec7b804c26ed108ebc65dab
Hischam El Yahiaoui <hiel1500@student.miun.se>
2016-09-09 10:55:51 +0200

    Hischam

---
+++ Grupp3 - Förskolan Drutten/Frånvaro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp3___Förskolan_Drutten
+{
+    class Frånvaro
+    {
+        public DateTime Datum { get; set; }
+        public string Förnamn { get; set; }
+        public string Efternamn { get; set; }
+        public bool Sjuk { get; set; }
+        public bool Ledig { get; set; }
+
+
+        public override string ToString()
+        {
+            return Datum + " " + Förnamn + " " + Efternamn + " " + Sjuk + " " + Ledig;
+        }
+
+    }
+}
