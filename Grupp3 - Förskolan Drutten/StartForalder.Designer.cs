1c22fb0e158fa48db731a73d1f30641d1feb44f3
mathildaw <m.athildaa@hotmail.com>
2016-09-13 13:59:20 +0200

    Lagt till så att tiderna kommer ut i comboboxarna

--- Grupp3 - Förskolan Drutten/StartForalder.Designer.cs
+++ Grupp3 - Förskolan Drutten/StartForalder.Designer.cs
@@ -41,10 +41,8 @@
             this.TidertabControl = new System.Windows.Forms.TabControl();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.textBoxFrån = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
             this.button5 = new System.Windows.Forms.Button();
-            this.textBoxUppdateraTill = new System.Windows.Forms.TextBox();
             this.label22 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -104,6 +102,12 @@
             this.comboBoxTill2 = new System.Windows.Forms.ComboBox();
             this.comboBoxTill1 = new System.Windows.Forms.ComboBox();
             this.label17 = new System.Windows.Forms.Label();
+            this.comboBoxUppdateraFrån1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxUppdateraTill1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxUppdateraFrån2 = new System.Windows.Forms.ComboBox();
+            this.comboBoxUppdateraTill2 = new System.Windows.Forms.ComboBox();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label24 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.TidertabControl.SuspendLayout();
@@ -330,24 +334,21 @@
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.textBoxFrån);
+            this.groupBox3.Controls.Add(this.comboBoxUppdateraTill2);
+            this.groupBox3.Controls.Add(this.comboBoxUppdateraFrån2);
+            this.groupBox3.Controls.Add(this.comboBoxUppdateraTill1);
+            this.groupBox3.Controls.Add(this.comboBoxUppdateraFrån1);
             this.groupBox3.Controls.Add(this.label9);
             this.groupBox3.Controls.Add(this.button5);
-            this.groupBox3.Controls.Add(this.textBoxUppdateraTill);
             this.groupBox3.Controls.Add(this.label22);
+            this.groupBox3.Controls.Add(this.label24);
+            this.groupBox3.Controls.Add(this.label23);
             this.groupBox3.Location = new System.Drawing.Point(491, 167);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(154, 159);
             this.groupBox3.TabIndex = 13;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Uppdatera befintliga tider";
-            // 
-            // textBoxFrån
-            // 
-            this.textBoxFrån.Location = new System.Drawing.Point(15, 36);
-            this.textBoxFrån.Name = "textBoxFrån";
-            this.textBoxFrån.Size = new System.Drawing.Size(100, 20);
-            this.textBoxFrån.TabIndex = 6;
             // 
             // label9
             // 
@@ -367,13 +368,6 @@
             this.button5.Text = "Uppdatera";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // textBoxUppdateraTill
-            // 
-            this.textBoxUppdateraTill.Location = new System.Drawing.Point(15, 87);
-            this.textBoxUppdateraTill.Name = "textBoxUppdateraTill";
-            this.textBoxUppdateraTill.Size = new System.Drawing.Size(100, 20);
-            this.textBoxUppdateraTill.TabIndex = 8;
             // 
             // label22
             // 
@@ -1010,6 +1004,112 @@
             this.label17.TabIndex = 19;
             this.label17.Text = ":";
             // 
+            // comboBoxUppdateraFrån1
+            // 
+            this.comboBoxUppdateraFrån1.FormattingEnabled = true;
+            this.comboBoxUppdateraFrån1.Items.AddRange(new object[] {
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18"});
+            this.comboBoxUppdateraFrån1.Location = new System.Drawing.Point(14, 37);
+            this.comboBoxUppdateraFrån1.Name = "comboBoxUppdateraFrån1";
+            this.comboBoxUppdateraFrån1.Size = new System.Drawing.Size(42, 21);
+            this.comboBoxUppdateraFrån1.TabIndex = 15;
+            // 
+            // comboBoxUppdateraTill1
+            // 
+            this.comboBoxUppdateraTill1.FormattingEnabled = true;
+            this.comboBoxUppdateraTill1.Items.AddRange(new object[] {
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18"});
+            this.comboBoxUppdateraTill1.Location = new System.Drawing.Point(14, 87);
+            this.comboBoxUppdateraTill1.Name = "comboBoxUppdateraTill1";
+            this.comboBoxUppdateraTill1.Size = new System.Drawing.Size(42, 21);
+            this.comboBoxUppdateraTill1.TabIndex = 16;
+            // 
+            // comboBoxUppdateraFrån2
+            // 
+            this.comboBoxUppdateraFrån2.FormattingEnabled = true;
+            this.comboBoxUppdateraFrån2.Items.AddRange(new object[] {
+            "00",
+            "05",
+            "10",
+            "15",
+            "20",
+            "25",
+            "30",
+            "35",
+            "40",
+            "45",
+            "50",
+            "55"});
+            this.comboBoxUppdateraFrån2.Location = new System.Drawing.Point(63, 37);
+            this.comboBoxUppdateraFrån2.Name = "comboBoxUppdateraFrån2";
+            this.comboBoxUppdateraFrån2.Size = new System.Drawing.Size(42, 21);
+            this.comboBoxUppdateraFrån2.TabIndex = 17;
+            // 
+            // comboBoxUppdateraTill2
+            // 
+            this.comboBoxUppdateraTill2.FormattingEnabled = true;
+            this.comboBoxUppdateraTill2.Items.AddRange(new object[] {
+            "00",
+            "05",
+            "10",
+            "15",
+            "20",
+            "25",
+            "30",
+            "35",
+            "40",
+            "45",
+            "50",
+            "55"});
+            this.comboBoxUppdateraTill2.Location = new System.Drawing.Point(63, 87);
+            this.comboBoxUppdateraTill2.Name = "comboBoxUppdateraTill2";
+            this.comboBoxUppdateraTill2.Size = new System.Drawing.Size(42, 21);
+            this.comboBoxUppdateraTill2.TabIndex = 18;
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label23.Location = new System.Drawing.Point(54, 39);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(13, 17);
+            this.label23.TabIndex = 19;
+            this.label23.Text = ":";
+            // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label24.Location = new System.Drawing.Point(54, 88);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(13, 17);
+            this.label24.TabIndex = 20;
+            this.label24.Text = ":";
+            // 
             // StartForalder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -1121,10 +1221,8 @@
         private System.Windows.Forms.ListBox listBoxMeddelaHämtning;
         private System.Windows.Forms.ListBox listBoxMeddelaFrånvaro;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.TextBox textBoxFrån;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.TextBox textBoxUppdateraTill;
         private System.Windows.Forms.Label label22;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.ComboBox comboBoxFrån1;
@@ -1133,5 +1231,11 @@
         private System.Windows.Forms.ComboBox comboBoxTill2;
         private System.Windows.Forms.ComboBox comboBoxTill1;
         private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.ComboBox comboBoxUppdateraTill2;
+        private System.Windows.Forms.ComboBox comboBoxUppdateraFrån2;
+        private System.Windows.Forms.ComboBox comboBoxUppdateraTill1;
+        private System.Windows.Forms.ComboBox comboBoxUppdateraFrån1;
+        private System.Windows.Forms.Label label24;
+        private System.Windows.Forms.Label label23;
     }
 }
