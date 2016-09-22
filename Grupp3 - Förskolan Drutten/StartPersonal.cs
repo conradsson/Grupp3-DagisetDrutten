7bc43f6161a32dc26f7c0403adc10ea38391934c
Hischam El Yahiaoui <hiel1500@student.miun.se>
2016-09-22 13:46:31 +0200

    aa

--- Grupp3 - Förskolan Drutten/StartPersonal.cs
+++ Grupp3 - Förskolan Drutten/StartPersonal.cs
@@ -431,19 +431,6 @@ namespace Grupp3___Förskolan_Drutten
             int kollaTid2 = 06;
             int tid2 = 06;
 
-            string[] antalBarnArray = new string[12];
-
-            for (int i = 0; i < antalBarnArray.Length; i++)
-            {
-                Postgres p = new Postgres();
-
-                antalBarnArray[i] = p.HämtaDagensTiderHämtas(DateTime.Today, 7).ToString();
-
-                MessageBox.Show(antalBarnArray[i]);
-
-                p.StängConnection();
-            }
-
             //MessageBox.Show(p.HämtaDagensTider(DateTime.Today, 7).ToString());
 
             for (int i = 0; i < 11; i++)
